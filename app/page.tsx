a340c4ec8ad190df67b1f80de0eada5814f2204e
Shaquille Blackwood <130801863+shaq4dev@users.noreply.github.com>
2024-10-31 12:03:37 -0400

    clerk authentication connected

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>AI clone App</h1>
+      <Button>Click Here!</Button>
+    </main>
+  );
+}
